afe5696a5a60070001152637a31fb9d11c28957d
star0202 <61998307+star0202@users.noreply.github.com>
2021-06-29 09:36:46 +0900

    Update

--- asset/js/randomimage.js
+++ asset/js/randomimage.js
@@ -1,8 +1,8 @@
 var imageURLs = [
-    "/asset/image/logo.png"
+    "/asset/image/logo.png"//이스터에그로 가끔 다른거 나오게 할거임
 ];
 function randomimage(){
-    var img = '<img src=\"';
+    var img = '<img align=\"center\" src=\"';
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
     img += imageURLs[randomIndex];
     img += '\" alt=\"Cute!!\"/>';
